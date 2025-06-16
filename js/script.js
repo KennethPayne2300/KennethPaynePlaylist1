cc29ff1e7c25babc8145f4a57cc3b1fd2f202e9e
KennethPayne2300 <kennethpayne353@gmail.com>
2025-06-16 15:35:19 -0500

    started playlist

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 // Complete Day 1 goals here
+let titlediv = document.querySelector(".songs");
+let artistdiv = document.querySelector(".artists");
+let titleinput = document.querySelector(".title");
+let artistinput = document.querySelector(".artist");
+let imageinput = document.querySelector(".image");
+let lengthinput = document.querySelector(".length");
+let linkinput = document.querySelector(".link");
+let add = document.querySelector(".add-input");
+
+let songs = [];
+let artists = [];
+let images = [];
+let lengths = [];
+let links = [];
 
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
@@ -10,17 +24,28 @@ function emptySongInfo() {
   too many times. Try commenting out this function call to see 
   what happens without it! */
   $(".songs").empty();
-  $(".images").empty();
   $(".artists").empty();
+  $(".images").empty();
   $(".lengths").empty();
   $(".links").empty();
 }
 
 function addSongInfo() {
   // Complete Day 3 goals inside this function
+  let title = titleinput.value;
+  let artist = artistinput.value;
+  let image = imageinput.value;
+  let length = lengthinput.value;
+  let link = linkinput.value;
+
+  songs.push(title);
+  artists.push(artist);
+  images.push(image);
+  lengths.push(length);
+  links.push(link);
 }
 
-$("#add").click(function () {
+add.addEventListener("click", function () {
   emptySongInfo();
   addSongInfo();
   displaySongInfo();
