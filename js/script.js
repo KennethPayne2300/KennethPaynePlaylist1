d2de2dcc6df600afad73702d393197bc15d3649a
KennethPayne2300 <kennethpayne353@gmail.com>
2025-06-17 12:57:35 -0500

    finished display

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 // Complete Day 1 goals here
-let titlediv = document.querySelector(".songs");
-let artistdiv = document.querySelector(".artists");
 let titleinput = document.querySelector(".title");
 let artistinput = document.querySelector(".artist");
 let imageinput = document.querySelector(".image");
@@ -8,7 +6,7 @@ let lengthinput = document.querySelector(".length");
 let linkinput = document.querySelector(".link");
 let add = document.querySelector(".add-input");
 
-let songs = [];
+let titles = [];
 let artists = [];
 let images = [];
 let lengths = [];
@@ -16,7 +14,34 @@ let links = [];
 
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
+  for(let i = 0; i<titles.length; i++) {
+    $(".songs").append(`<p>${titles[i]}</p>`);
+    $(".artists").append(`<p>${artists[i]}</p>`);
+    $(".images").append(`<img src="${images[i]}" alt="cover image for ${titles[i]}">`);
+    $(".lengths").append(`<p>${lengths[i]}</p>`);
+    $(".links").append(`<a href="${links[i]}" target="_blank">Listen here</a>`);
+    $(".delete").append(`<button class="delete-button" onclick="deleteSong(${i})">Delete</button>`);
+  }
 }
+
+function deleteSong(index) {
+  titles.splice(index, 1);
+  artists.splice(index, 1);
+  images.splice(index, 1);
+  lengths.splice(index, 1);
+  links.splice(index, 1);
+  
+  emptySongInfo();
+  
+  // Re-display the updated song information
+  for(let i = 0; i<titles.length; i++) {
+    $(".songs").append(`<p>${titles[i]}</p>`);
+    $(".artists").append(`<p>${artists[i]}</p>`);
+    $(".images").append(`<img src="${images[i]}" alt="cover image for ${titles[i]}">`);
+    $(".lengths").append(`<p>${lengths[i]}</p>`);
+    $(".links").append(`<a href="${links[i]}">Listen here</a>`);
+    $(".delete").append(`<button class="delete-button" onclick="deleteSong(${i})">Delete</button>`);
+  }}
 
 function emptySongInfo() {
   /* this function empties the divs each time the button is 
@@ -28,6 +53,7 @@ function emptySongInfo() {
   $(".images").empty();
   $(".lengths").empty();
   $(".links").empty();
+  $(".delete").empty();
 }
 
 function addSongInfo() {
@@ -38,7 +64,7 @@ function addSongInfo() {
   let length = lengthinput.value;
   let link = linkinput.value;
 
-  songs.push(title);
+  titles.push(title);
   artists.push(artist);
   images.push(image);
   lengths.push(length);
